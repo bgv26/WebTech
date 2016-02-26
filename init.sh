babce9893a41532544a2798c1c2ed8a4223dc371
bgv26 <bgv26@users.noreply.github.com>
2016-02-27 02:14:36 +0300

    Update init.sh
    Add condition for creating directory structure

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
-mkdir -R web/public/{img,css,js}
-mkdir web/uploads
+if [ ! -d "public" ]; then
+    mkdir public
+    mkdir public/{img,css,js}
+fi
+if [ ! -d "uploads" ]; then
+    mkdir uploads
+fi
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
