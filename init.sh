85d851df6dbae4231d8502592f86a828600db9e8
bgv26 <bgv26@users.noreply.github.com>
2016-02-27 02:06:07 +0300

    Update init.sh
    Add creating directory and change link nginx.conf to default

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+mkdir -R web/public/{img,css,js}
+mkdir web/uploads
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
