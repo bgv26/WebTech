b432eecf2e38ad2c223a57e96a975585752bab3d
bgv26 <bgv26@users.noreply.github.com>
2016-02-28 17:07:56 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python
-from urlparse import parse_qs
+from urlparse import parse_qsl
 
 def app(env, start_response):
    start_response('200 OK', [('Content-Type', 'text/plain')])
    returnString = ''
-   for (key,value) in parse_qs(env['QUERY_STRING'], keep_blank_values=True).iteritems():
+   for (key, value) in parse_qsl(env['QUERY_STRING'], keep_blank_values=True):
       returnString += key + '=' + value + '\n'
    return returnString
    
