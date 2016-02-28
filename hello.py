0b37d922ee99bd15fa619561b7941599c628b7ed
bgv26 <bgv26@users.noreply.github.com>
2016-02-28 16:58:03 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 from urlparse import parse_qsl
 
 def app(env, start_response):
-   start_response('200 OK', [('Content-Type', 'plain/text')])
+   start_response('200 OK', [('Content-Type', 'text/plain')])
    returnString = ''
    for qsi in parse_qsl(env['QUERY_STRING'], keep_blank_values=True):
       returnString += qsi[0] + '=' + qsi[1] + '\n'
