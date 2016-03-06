8425ea0894760ef7380e634e9ea03d8cb3a9fe52
bgv26 <bgv26@mail.ru>
2016-03-06 20:36:10 +0000

    Add ask django project

---
+++ ask/qa/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^/$', 'views.test'),
+    url(r'^/login/$', 'views.test'),
+    url(r'^/signup/$', 'views.test'),
+    url(r'^/question/\d+/$', 'views.test'),
+    url(r'^/ask/$', 'views.test'),
+    url(r'^/popular/$', 'views.test'),
+    url(r'^/new/$', 'views.test'),
+)
