205dd061ecd57217fc20b759cdc6f36e99ab42ee
bgv26 <bgv26@mail.ru>
2016-03-09 07:41:39 +0300

    Add models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = User()
+    likes = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.title
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question)
+    author = User()
+
+    def __unicode__(self):
+        return self.question
