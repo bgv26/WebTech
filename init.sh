7e7d30de5db354604785583281ffc11b19886252
bgv26 <bgv26@mail.ru>
2016-03-09 11:33:55 +0300

    Add views for index, popular and question

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ fi
 if [ ! -d "uploads" ]; then
     mkdir uploads
 fi
+sudo /etc/init.d/mysql restart
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
